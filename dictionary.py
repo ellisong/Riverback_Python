87f9eada778715c179d73d5bcaf0212d9c91fd7e
Grady W. Ellison <ellisong1990@gmail.com>
2015-11-11 19:54:22 -0800

    Fixed bugs in Dictionary. added setMultiple method

--- dictionary.py
+++ dictionary.py
@@ -1,16 +1,22 @@
 class Dictionary():
     _dict = {}
     
-    def get(key=None):
+    def get(self, key=None):
         if not key:
-            return _dict
+            return self._dict
         if key in self._dict:
             return self._dict[key]
         return None
     
-    def set(key, value):
+    def set(self, key, value):
         self._dict[key] = value
     
-    def delete(key):
+    def delete(self, key):
         if key in self._dict:
             del self._dict[key]
+    
+    def setMultiple(self, list, prefix, separator='_', start=0):
+        cntr = start
+        for ii in list:
+            self.set('{0}{1}{2}'.format(prefix, separator, str(cntr)), ii)
+            cntr = cntr + 1
