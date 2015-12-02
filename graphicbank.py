cb8224794240ac64ffe15bc0a8a3bb9377ac597e
Grady W. Ellison <ellisong1990@gmail.com>
2015-12-01 21:48:47 -0800

    The graphic bank image can now be drawn to with the graphic bank's own tile data

--- graphicbank.py
+++ graphicbank.py
@@ -12,6 +12,7 @@ class GraphicBank():
     tileAmount = 0
     paletteAmount = 0
     paletteColorAmount = 0
+    bankHasPalettes = True
     
     data = []
     image = None
@@ -29,6 +30,9 @@ class GraphicBank():
         self.paletteAmount = int(treeRoot.find('bank/paletteamount').text)
         self.paletteColorAmount = int(treeRoot.find('palette/coloramount').text)
     
+    def setBankHasPalettes(val):
+        self.bankHasPalettes = val
+    
     def getData(self):
         return self.data
     
@@ -41,19 +45,33 @@ class GraphicBank():
         assert(self.tileAmount > 0)
         self.image = Image.new('RGB', (self.tileWidth*self.tileAmount, self.tileHeight))
     
-    def getTileImage(self, tileNumber, palette, bankHasPalettes=True):
+    def updateImage(self, palette):
         assert(self.tileWidth > 0)
         assert(self.tileHeight > 0)
-        planarTileData = self.getPlanarTileFromBankData(tileNumber, bankHasPalettes)
+        for tileNumber in range(0, self.tileAmount):
+            self.drawTileOnImage(tileNumber, palette)
+        
+    def drawTileOnImage(self, tileNumber, palette):
+        assert(self.tileWidth > 0)
+        assert(self.tileHeight > 0)
+        assert((tileNumber >= 0) and (tileNumber < self.tileAmount))
+        tileimg = self.getTileImage(tileNumber, palette)
+        self.image.paste(tileimg, (self.tileWidth*tileNumber, 0))
+    
+    def getTileImage(self, tileNumber, palette):
+        assert(self.tileWidth > 0)
+        assert(self.tileHeight > 0)
+        assert((tileNumber >= 0) and (tileNumber < self.tileAmount))
+        planarTileData = self.getPlanarTileFromBankData(tileNumber)
         linearTileData = ImageEditor.convertPlanarTileToLinearTile(planarTileData)
         coloredTileData = ImageEditor.colorLinearTileWithPalette(linearTileData, palette)
         return ImageEditor.createImageFromColoredLinearTile(coloredTileData, self.tileWidth, self.tileHeight)
     
-    def getPlanarTileFromBankData(self, tileNumber, hasPalettes=True):
+    def getPlanarTileFromBankData(self, tileNumber):
         assert(len(self.data) > 0)
         assert(tileNumber >= 0)
         offset = 0
-        if hasPalettes:
+        if self.bankHasPalettes:
             offset = 0x1E0
         offset += tileNumber * 0x20
         return self.data[offset:offset+0x20]
