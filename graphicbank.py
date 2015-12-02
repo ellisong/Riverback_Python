110583436de7e174b2fdde39316fc82e6631bd19
Grady W. Ellison <ellisong1990@gmail.com>
2015-12-01 21:18:59 -0800

    Added a graphic bank class which stores graphic tile data. ImageEditor can now create a tile image from the graphics of the ROM.

---
+++ graphicbank.py
@@ -0,0 +1,76 @@
+from PIL import Image, ImageTk
+import xml.etree.ElementTree
+from imageeditor import ImageEditor
+from palette import Palette
+from imageeditor import ImageEditor
+from palette import Palette
+from color import Color
+
+class GraphicBank():
+    tileWidth = 0
+    tileHeight = 0
+    tileAmount = 0
+    paletteAmount = 0
+    paletteColorAmount = 0
+    
+    data = []
+    image = None
+    
+    def __init__(self, treeRoot=None, data=None):
+        if treeRoot is not None:
+            self.readConfig(treeRoot)
+        self.data = data
+        self.createImage()
+    
+    def readConfig(self, treeRoot):
+        self.tileWidth = int(treeRoot.find('tile/width').text)
+        self.tileHeight = int(treeRoot.find('tile/height').text)
+        self.tileAmount = int(treeRoot.find('bank/tileamount').text)
+        self.paletteAmount = int(treeRoot.find('bank/paletteamount').text)
+        self.paletteColorAmount = int(treeRoot.find('palette/coloramount').text)
+    
+    def getData(self):
+        return self.data
+    
+    def getImage(self):
+        return self.image
+    
+    def createImage(self):
+        assert(self.tileWidth > 0)
+        assert(self.tileHeight > 0)
+        assert(self.tileAmount > 0)
+        self.image = Image.new('RGB', (self.tileWidth*self.tileAmount, self.tileHeight))
+    
+    def getTileImage(self, tileNumber, palette, bankHasPalettes=True):
+        assert(self.tileWidth > 0)
+        assert(self.tileHeight > 0)
+        planarTileData = self.getPlanarTileFromBankData(tileNumber, bankHasPalettes)
+        linearTileData = ImageEditor.convertPlanarTileToLinearTile(planarTileData)
+        coloredTileData = ImageEditor.colorLinearTileWithPalette(linearTileData, palette)
+        return ImageEditor.createImageFromColoredLinearTile(coloredTileData, self.tileWidth, self.tileHeight)
+    
+    def getPlanarTileFromBankData(self, tileNumber, hasPalettes=True):
+        assert(len(self.data) > 0)
+        assert(tileNumber >= 0)
+        offset = 0
+        if hasPalettes:
+            offset = 0x1E0
+        offset += tileNumber * 0x20
+        return self.data[offset:offset+0x20]
+    
+    def getPalettesFromBankData(self):
+        assert(self.paletteColorAmount > 0)
+        assert(self.paletteAmount > 0)
+        paletteList = []
+        pointer = 0
+        for palNum in range(0, self.paletteAmount):
+            pal = Palette(False)
+            for colorNum in range(0, self.paletteColorAmount):
+                B = ((self.data[pointer+1] & 0b01111100) >> 2)
+                G = (((self.data[pointer+1] & 0b00000011) << 3) + ((self.data[pointer] & 0b11100000) >> 5))
+                R = self.data[pointer] & 0b00011111
+                pal.append(Color(red=R, green=G, blue=B, type=False))
+                pointer += 2
+            pal.switchType()
+            paletteList.append(pal)
+        return paletteList 
--- imageeditor.py
+++ imageeditor.py
@@ -1,18 +1,25 @@
 from PIL import Image, ImageTk
+import xml.etree.ElementTree
+from math import pow
+from palette import Palette
+from color import Color
 
-class ImageEditor():
+class ImageEditor():  
     def getTileFromTileset(image, tileX, tileY, tileWidth, tileHeight):
         assert(tileX >= 0)
         assert(tileY >= 0)
         assert(tileWidth >= 1)
         assert(tileHeight >= 1)
+        assert(tileX*tileWidth+tileWidth < image.width)
+        assert(tileY*tileHeight+tileHeight < image.height)
         return image.crop((tileX*tileWidth, tileY*tileHeight, tileX*tileWidth+tileWidth, tileY*tileHeight+tileHeight))
     
     def getTilesListFromTileset(image, tileAmount, tileWidth, tileHeight):
+        assert(image.width > 0)
+        assert(image.height > 0)
         assert(tileWidth >= 1)
         assert(tileHeight >= 1)
         assert(tileAmount >= 1)
-        
         tiles = []
         x = 0
         y = 0
@@ -20,24 +27,77 @@ class ImageEditor():
             for x in range(0, image.width // tileWidth):
                 if len(tiles) >= tileAmount:
                     return tiles
-                tiles.append(ImageEditor.getTileFromTileset(image, x, y, tileWidth, tileHeight))
+                tiles.append(ImageEditor.getTileFromTileset(image, x, y))
         return tiles
     
-    def createTransposedImage(image, horizontal, vertical, rotation):
+    def convertPlanarTileToLinearTile(tiledata):
+        assert(len(tiledata) >= 32)
+        bitplane = []
+        offset = 0
+        for planeNum in range(0, 4):
+            plane = bytearray()
+            if (planeNum == 2):
+                offset = 16
+            for byte in range(0, 8):
+                bitmask = 0x80
+                for bit in range(0, 8):
+                    bitValue = (tiledata[offset+(byte*2)] & bitmask)
+                    if (bitValue > 0):
+                        bitValue = 1
+                    plane.append(bitValue * (0x01 << planeNum))
+                    bitmask = bitmask >> 1
+            bitplane.append(plane)
+            offset += 1
+        lineardata = bytearray()
+        pix = 0
+        for pixel in range(0, 64):
+            if ((pixel % 2) == 0):
+                pix = 0
+            for planeNum in range(0, 4):
+                pix += (bitplane[planeNum])[pixel]
+            if ((pixel % 2) == 0):
+                pix = pix << 4
+            else:
+                lineardata.append(pix)
+        return lineardata
+    
+    def colorLinearTileWithPalette(tiledata, palette):
+        assert(len(tiledata) >= 32)
+        pointer = 0
+        colors = []
+        while (pointer < 32):
+            tile = (tiledata[pointer] & 0xF0) >> 4
+            colors.append(palette.getColors()[tile])
+            tile = tiledata[pointer] & 0x0F
+            colors.append(palette.getColors()[tile])
+            pointer += 1
+        return colors
+    
+    def createImageFromColoredLinearTile(tiledata, width, height):
+        assert(width > 0)
+        assert(height > 0)
+        imagebytes = bytearray()
+        for col in tiledata:
+            imagebytes.append(col.getRed())
+            imagebytes.append(col.getGreen())
+            imagebytes.append(col.getBlue())
+        return Image.frombytes('RGB', (width, height), bytes(imagebytes))
+                
+    def createTransposedImage(image, horizontal=False, vertical=False, rotation=0):
         img = image
         if horizontal:
             img = img.transpose(Image.FLIP_LEFT_RIGHT)
         if vertical:
             img = img.transpose(Image.FLIP_TOP_BOTTOM)
-        if rotation == 90:
+        if ((rotation % 360) == 90):
             img = img.transpose(Image.ROTATE_90)
-        elif rotation == 180:
+        elif ((rotation % 360) == 180):
             img = img.transpose(Image.ROTATE_180)
-        elif rotation == 270:
+        elif ((rotation % 360) == 270):
             img = img.transpose(Image.ROTATE_270)
         return img
     
-    def createTransposedImagesFromList(imageList, horizontal, vertical, rotation):
+    def createTransposedImagesFromList(imageList, horizontal=False, vertical=False, rotation=0):
         convertedList = []
         for img in imageList:
             convertedList.append(ImageEditor.createTransposedImage(img, horizontal, vertical, rotation))
